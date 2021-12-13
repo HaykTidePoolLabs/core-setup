dab44f7b2c93b98e0c31f524599d4ac1c232ece0
haykmuradyan <hayk@tidepoollabs.com>
2021-12-13 12:43:27 +0400

    initial commit

--- graphql/main/src/app.module.ts
+++ graphql/main/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import {APP_INTERCEPTOR} from "@nestjs/core";
+import {GraphqlInterceptor} from "@ntegral/nestjs-sentry";
 import { configValidationSchema } from './config/config.schema';
+import {SentryProviderModule} from "./providers/sentry";
 import { RedisCacheProviderModule } from "./providers/cache/redis";
 import { PostgresDatabaseProviderModule } from './providers/database/Postgres';
 import { GraphGLProviderModule } from './providers/graphql';
 import { MicroservicesProviderModule } from './providers/microservices';
 import { UserModule } from './modules/user/user.module';
 import { PostsModule } from './modules/posts/posts.module';
+
 
 @Module({
     imports: [
@@ -15,6 +19,7 @@ import { PostsModule } from './modules/posts/posts.module';
             validationSchema: configValidationSchema,
             isGlobal: true,
         }),
+        SentryProviderModule,
         RedisCacheProviderModule,
         MicroservicesProviderModule,
         PostgresDatabaseProviderModule,
@@ -22,6 +27,11 @@ import { PostsModule } from './modules/posts/posts.module';
         UserModule,
         PostsModule,
     ],
-    providers: [],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useFactory: () => new GraphqlInterceptor(),
+        },
+    ],
 })
 export class AppModule {}
