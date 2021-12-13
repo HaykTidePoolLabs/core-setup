14275cf1e808749f837466a528da34cb4f4dc54d
haykmuradyan <hayk@tidepoollabs.com>
2021-12-13 15:42:34 +0400

    initial commit

--- cron/main/src/modules/user/user.service.ts
+++ cron/main/src/modules/user/user.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, UseInterceptors} from '@nestjs/common';
 import {Cron} from "@nestjs/schedule";
-
+import {SentryOverwatchAsync} from "../../common/decorators/sentry-overwatch.decorator";
 
 @Injectable()
 export class UserService {
@@ -11,7 +11,9 @@ export class UserService {
 
   @Cron('*/3 * * * * *', {
   })
+  @SentryOverwatchAsync()
   triggerNotifications() {
+    // throw new Error('ooo')
     console.log('aaa', new  Date())
   }
 }
