efd86d8de48e0e0b2645fbf41a559a31633251ca
haykmuradyan <hayk@tidepoollabs.com>
2021-12-14 13:39:12 +0400

    initial commit

--- rest/main/src/config/sentry/index.ts
+++ rest/main/src/config/sentry/index.ts
@@ -1,9 +1,13 @@
 import { ConfigService } from "@nestjs/config";
 import {SentryModuleOptions} from '@ntegral/nestjs-sentry';
 import {LogLevel} from "@sentry/types";
+import { Integrations } from "@sentry/tracing";
 
 export const sentryConfig = (configService: ConfigService): SentryModuleOptions => {
     return {
+        integrations: [
+            new Integrations.Express()
+        ],
         dsn: configService.get('SENTRY_DSN'),
         environment: configService.get('SENTRY_ENV'),
         release: configService.get('SENTRY_RELEASE'),
